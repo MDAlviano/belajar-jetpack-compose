91ff5e7908e98e38f0999bd0cd234b650d05a1c4
MDAlviano <mdalviano.21122@gmail.com>
2025-01-02 10:14:52 +0700

    bottom navigation bar pt.1

---
+++ app/src/main/java/com/alvin/belajar/compose/bottom_navigation/MainScreen.kt
@@ -0,0 +1,34 @@
+package com.alvin.belajar.compose.bottom_navigation
+
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {}
+    ) {
+        BottomNavGraph(navController = navController)
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Settings
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+
+    }
+}
